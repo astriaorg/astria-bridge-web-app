52e04d2a44f803860ea5c4eb41a943f9095c90a0
Jesse Snyder <jessetsnyder@gmail.com>
2024-05-30 12:00:18 -0600

    fix tests

--- web/src/testHelpers.tsx
+++ web/src/testHelpers.tsx
@@ -1,13 +1,16 @@
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { render } from "@testing-library/react";
 import type React from "react";
+import { EthWalletContextProvider } from "./features/EthWallet/contexts/EthWalletContext";
 
 export const renderWithRouter = (element: React.JSX.Element) => {
   render(
     <MemoryRouter>
-      <Routes>
-        <Route index path={"*"} element={element} />
-      </Routes>
+      <EthWalletContextProvider>
+        <Routes>
+          <Route index path={"*"} element={element} />
+        </Routes>
+      </EthWalletContextProvider>
     </MemoryRouter>,
   );
 };
