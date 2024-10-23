676bc68127a02bb4f60beffff7c2c2c673fbbc03
jesse snyder <jessetsnyder@gmail.com>
2024-10-23 12:24:25 -0600

    Release 10/23/24-2 (#26)
    * use fakeTIA
    
    * rename contract address properties to be more clear (#23)
    
    * add noble to dawn configs (#25)

--- web/src/config/chainConfigs/ChainConfigsDawn.ts
+++ web/src/config/chainConfigs/ChainConfigsDawn.ts
@@ -82,8 +82,91 @@ const CelestiaChainInfo: IbcChainInfo = {
   iconClass: "i-celestia",
 };
 
+const NobleChainInfo: IbcChainInfo = {
+  chainId: "grand-1",
+  chainName: "Noble Testnet",
+  // RPC endpoint of the chain
+  // rpc: "https://rpc.testnet.noble.strange.love:443",
+  // rpc: "https://rpc.testnet.noble.strange.love",
+  rpc: "https://noble-testnet-rpc.polkachu.com",
+  // REST endpoint of the chain.
+  rest: "https://noble-testnet-api.polkachu.com",
+  // Staking coin information
+  stakeCurrency: {
+    // Coin denomination to be displayed to the user.
+    coinDenom: "USDC",
+    // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+    coinMinimalDenom: "uusdc",
+    // # of decimal points to convert minimal denomination to user-facing denomination.
+    coinDecimals: 6,
+    // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+    // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+    // coinGeckoId: ""
+  },
+  // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
+  // The 'stake' button in Keplr extension will link to the webpage.
+  // walletUrlForStaking: "",
+  // The BIP44 path.
+  bip44: {
+    // You can only set the coin type of BIP44.
+    // 'Purpose' is fixed to 44.
+    coinType: 118,
+  },
+  // The address prefix of the chain.
+  bech32Config: {
+    bech32PrefixAccAddr: "noble",
+    bech32PrefixAccPub: "noblepub",
+    bech32PrefixConsAddr: "noblevalcons",
+    bech32PrefixConsPub: "noblevalconspub",
+    bech32PrefixValAddr: "noblevaloper",
+    bech32PrefixValPub: "noblevaloperpub",
+  },
+  // List of all coin/tokens used in this chain.
+  currencies: [
+    {
+      // Coin denomination to be displayed to the user.
+      coinDenom: "USDC",
+      // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+      coinMinimalDenom: "uusdc",
+      // # of decimal points to convert minimal denomination to user-facing denomination.
+      coinDecimals: 6,
+      // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+      // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+      // coinGeckoId: ""
+      ibcChannel: "channel-232",
+      sequencerBridgeAccount: "astria1u6ewl0tejz0df2l6tzc7k2degx6mqsjahldqxd",
+      iconClass: "i-noble",
+    },
+  ],
+  // List of coin/tokens used as a fee token in this chain.
+  feeCurrencies: [
+    {
+      // Coin denomination to be displayed to the user.
+      coinDenom: "USDC",
+      // Actual denom (i.e. nria, uscrt) used by the blockchain.
+      coinMinimalDenom: "usdc",
+      // # of decimal points to convert minimal denomination to user-facing denomination.
+      coinDecimals: 6,
+      // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+      // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+      // coinGeckoId: ""
+      // (Optional) This is used to set the fee of the transaction.
+      // If this field is not provided and suggesting chain is not natively integrated, Keplr extension will set the Keplr default gas price (low: 0.01, average: 0.025, high: 0.04).
+      // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
+      // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
+      gasPriceStep: {
+        low: 0.01,
+        average: 0.02,
+        high: 0.1,
+      },
+    },
+  ],
+  iconClass: "i-noble",
+};
+
 export const ibcChains: IbcChains = {
   "Celestia Mocha-4": CelestiaChainInfo,
+  "Noble Testnet": NobleChainInfo,
 };
 
 const FlameChainInfo: EvmChainInfo = {
@@ -98,6 +181,13 @@ const FlameChainInfo: EvmChainInfo = {
       nativeTokenWithdrawerContractAddress:
         "0x77Af806d724699B3644F9CCBFD45CC999CCC3d49",
       iconClass: "i-celestia",
+    },
+    {
+      coinDenom: "USDC",
+      coinMinimalDenom: "uusdc",
+      coinDecimals: 18,
+      erc20ContractAddress: "0x6e18cE6Ec3Fc7b8E3EcFca4fA35e25F3f6FA879a",
+      iconClass: "i-noble",
     },
   ],
   iconClass: "i-flame",
