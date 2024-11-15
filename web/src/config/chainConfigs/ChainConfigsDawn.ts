7e6a1cda738f1aa150e689fb8729f933c40b3764
ido <ido@astria.org>
2024-11-15 18:32:43 +0200

    add neutron as an ibc chain with dtia as an asset

--- web/src/config/chainConfigs/ChainConfigsDawn.ts
+++ web/src/config/chainConfigs/ChainConfigsDawn.ts
@@ -178,9 +178,92 @@ const NobleChainInfo: IbcChainInfo = {
   iconClass: "i-noble",
 };
 
+const NeutronChainInfo: IbcChainInfo = {
+  chainId: "pion-1",
+  chainName: "Neutron Testnet",
+  // RPC endpoint of the chain
+  // rpc: "https://rpc.testnet.noble.strange.love:443",
+  // rpc: "https://rpc.testnet.noble.strange.love",
+  rpc: "https://rpc-falcron.pion-1.ntrn.tech",
+  // REST endpoint of the chain.
+  rest: "https://rest-falcron.pion-1.ntrn.tech",
+  // Staking coin information
+  stakeCurrency: {
+    // Coin denomination to be displayed to the user.
+    coinDenom: "dTIA",
+    // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+    coinMinimalDenom: "factory/neutron1tkr6mtll5e2z53ze2urnc3ld3tq3dam2rchezc5lg9c237ft66gqtw94jm/drop",
+    // # of decimal points to convert minimal denomination to user-facing denomination.
+    coinDecimals: 6,
+    // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+    // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+    // coinGeckoId: ""
+  },
+  // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
+  // The 'stake' button in Keplr extension will link to the webpage.
+  // walletUrlForStaking: "",
+  // The BIP44 path.
+  bip44: {
+    // You can only set the coin type of BIP44.
+    // 'Purpose' is fixed to 44.
+    coinType: 118,
+  },
+  // The address prefix of the chain.
+  bech32Config: {
+    bech32PrefixAccAddr: "neutron",
+    bech32PrefixAccPub: "neutronpub",
+    bech32PrefixConsAddr: "neutronvalcons",
+    bech32PrefixConsPub: "neutronvalconspub",
+    bech32PrefixValAddr: "neutronvaloper",
+    bech32PrefixValPub: "neutronvaloperpub",
+  },
+  // List of all coin/tokens used in this chain.
+  currencies: [
+    {
+      // Coin denomination to be displayed to the user.
+      coinDenom: "dTIA",
+      // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+      coinMinimalDenom: "factory/neutron1tkr6mtll5e2z53ze2urnc3ld3tq3dam2rchezc5lg9c237ft66gqtw94jm/drop",
+      // # of decimal points to convert minimal denomination to user-facing denomination.
+      coinDecimals: 6,
+      // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+      // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+      // coinGeckoId: ""
+      ibcChannel: "channel-1412",
+      sequencerBridgeAccount: "astria1j7juyc9nv6tlv0la74a9rrm7v72y3x336mgxvk",
+      iconClass: "i-neutron",
+    },
+  ],
+  // List of coin/tokens used as a fee token in this chain.
+  feeCurrencies: [
+    {
+      // Coin denomination to be displayed to the user.
+      coinDenom: "dTIA",
+      // Actual denom (i.e. nria, uscrt) used by the blockchain.
+      coinMinimalDenom: "factory/neutron1tkr6mtll5e2z53ze2urnc3ld3tq3dam2rchezc5lg9c237ft66gqtw94jm/drop",
+      // # of decimal points to convert minimal denomination to user-facing denomination.
+      coinDecimals: 6,
+      // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+      // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+      // coinGeckoId: ""
+      // (Optional) This is used to set the fee of the transaction.
+      // If this field is not provided and suggesting chain is not natively integrated, Keplr extension will set the Keplr default gas price (low: 0.01, average: 0.025, high: 0.04).
+      // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
+      // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
+      gasPriceStep: {
+        low: 0.01,
+        average: 0.02,
+        high: 0.1,
+      },
+    },
+  ],
+  iconClass: "i-neutron",
+};
+
 export const ibcChains: IbcChains = {
   "Celestia Mocha-4": CelestiaChainInfo,
   "Noble Testnet": NobleChainInfo,
+  "Neutron Testnet": NeutronChainInfo,
 };
 
 const FlameChainInfo: EvmChainInfo = {
@@ -205,6 +288,14 @@ const FlameChainInfo: EvmChainInfo = {
       ibcWithdrawalFeeWei: "10000000000000000",
       iconClass: "i-noble",
     },
+    {
+      coinDenom: "dTIA",
+      coinMinimalDenom: "factory/neutron1tkr6mtll5e2z53ze2urnc3ld3tq3dam2rchezc5lg9c237ft66gqtw94jm/drop",
+      coinDecimals: 18,
+      erc20ContractAddress: "0x0F0C3207a9fE9B7e8AaE4bb83E865C91A13Fd8a7",
+      ibcWithdrawalFeeWei: "10000000000000000",
+      iconClass: "i-neutron",
+    }
   ],
   iconClass: "i-flame",
 };
